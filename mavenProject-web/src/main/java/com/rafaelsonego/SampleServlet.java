2cd48252980b2071fdf83529cdda19b1c437213a
Rafael Silva <rafael2785@gmail.com>
2018-04-24 22:37:19 -0400

    Use a class for the core project

--- mavenProject-web/src/main/java/com/rafaelsonego/SampleServlet.java
+++ mavenProject-web/src/main/java/com/rafaelsonego/SampleServlet.java
@@ -9,6 +9,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.rafaelsonego.pojo.User;
+
 @WebServlet(urlPatterns = { "/sampleServlet" })
 public class SampleServlet extends HttpServlet {
 
@@ -16,11 +18,12 @@ public class SampleServlet extends HttpServlet {
 	 * 
 	 */
 	private static final long serialVersionUID = -3177714591709998851L;
- 
+
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		User user = new User("Rafael Silva", 33);
 		PrintWriter writer = resp.getWriter();
-		writer.println("<html><h1>Rafael Sonego - Sample Servlet</h1></html>");
+		writer.println("<html><h1>" + user.getName() + " Age: " + user.getAge() + "</h1></html>");
 		writer.close();
 	}
 }
